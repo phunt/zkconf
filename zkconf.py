03fff562a7b5f0b1c14ae386c4cccbe7883acd5e
Patrick Hunt <phunt1@gmail.com>
2009-08-07 13:11:29 -0700

    added support for flex quorum

--- zkconf.py
+++ zkconf.py
@@ -13,34 +13,55 @@ from stop import stop
 usage = "usage: %prog [options] zookeeper_trunk_dir"
 parser = OptionParser(usage=usage)
 parser.add_option("-c", "--count", dest="count",
-                  default=3, help="ensemble size")
+                  default="3", help="ensemble size")
 parser.add_option("", "--clientportstart", dest="clientportstart",
-                  default=2180, help="first client port")
+                  default="2180", help="first client port")
 parser.add_option("", "--quorumportstart", dest="quorumportstart",
-                  default=3180, help="first quorum port")
+                  default="3180", help="first quorum port")
 parser.add_option("", "--electionportstart", dest="electionportstart",
-                  default=4180, help="first election port")
+                  default="4180", help="first election port")
+parser.add_option("", "--weights", dest="weights",
+                  default="1", help="comma separated list of weights for each server (flex quorum only)")
+parser.add_option("", "--groups", dest="groups",
+                  default="1", help="comma separated list of groups (flex quorum only)")
 
 (options, args) = parser.parse_args()
+
+options.count = int(options.count)
+options.clientportstart = int(options.clientportstart)
+options.quorumportstart = int(options.quorumportstart)
+options.electionportstart = int(options.electionportstart)
+
+if options.weights != "1" :
+    options.weights = options.weights.split(",")
+else :
+    options.weights = []
+
+if options.groups != "1" :
+    options.groups = options.groups.split(",")
+else :
+    options.groups = []
 
 if len(args) != 1:
     parser.error("need zookeeper_trunk_dir in order to get jars and conf")
 
 if __name__ == '__main__':
     serverlist = []
-    for sid in xrange(1, int(options.count) + 1) :
+    for sid in xrange(1, options.count + 1) :
         serverlist.append([sid,
                            options.clientportstart + sid,
                            options.quorumportstart + sid,
                            options.electionportstart + sid])
 
-    for sid in xrange(1, int(options.count) + 1) :
+    for sid in xrange(1, options.count + 1) :
         serverdir = "s" + str(sid)
         os.mkdir(serverdir)
         os.mkdir(os.path.join(serverdir, "data"))
         conf = zoocfg(searchList=[{'sid' : sid,
                                    'clientPort' :
                                        options.clientportstart + sid,
+                                   'weights' : options.weights,
+                                   'groups' : options.groups,
                                    'serverlist' : serverlist}])
         f = open(os.path.join(serverdir, "zoo.cfg"), 'w')
         f.write(str(conf))
